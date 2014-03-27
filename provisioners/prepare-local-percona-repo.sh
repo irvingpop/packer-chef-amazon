f05fb0e825b94a8b2f90761df0e2907b3c2cdfd3
Jay Janssen <jay.janssen@gmail.com>
2014-03-27 10:02:27 -0400

    cleaned up repo fetch

--- provisioners/prepare-local-percona-repo.sh
+++ provisioners/prepare-local-percona-repo.sh
@@ -9,7 +9,7 @@
 # - yum localinstall --download-only to get all deps too
 # - set a local.repo, can be disabled via puppet if desired
 
-importantpkgs=( "Percona-XtraDB-Cluster-full-56" "percona-xtrabackup" "Percona-Server-server-55" "Percona-Server-server-56", "Percona-Server-shared-51" )
+importantpkgs=( "Percona-XtraDB-Cluster-full-56" "percona-xtrabackup" "Percona-Server-server-55" "Percona-Server-server-56" "Percona-Server-shared-51" )
 extrapkgs=( "haproxy" "xinetd" "keepalived" )
 
 # This isn't perfect, but good enough for now
@@ -24,14 +24,14 @@ if [ -x /usr/bin/yum ]; then
 
 	# Get (most) rpm packages from Percona repo
 	mkdir -p /var/repo
-	reposync -n --repoid=percona -p /var/repo
+	reposync -n --repoid=percona --norepopath -p /var/repo
 
 	# Clean out unnecessary packages
-	rm -rf /var/repo/percona/*-50-*
-	rm -rf /var/repo/percona/*-51-*
-	rm -rf /var/repo/percona/Percona-XtraDB-Cluster-*-5.5.34-23*
-	rm -rf /var/repo/percona/*-debuginfo-*
-	rm -rf /var/repo/percona/*-test-*
+	rm -rf /var/repo/*-50-*
+	rm -rf /var/repo/*-51-*
+	rm -rf /var/repo/Percona-XtraDB-Cluster-*-5.5.34-23*
+	rm -rf /var/repo/*-debuginfo-*
+	rm -rf /var/repo/*-test-*
 
 	# Create local repo temporarily
 	createrepo /var/repo
@@ -43,19 +43,18 @@ baseurl=file:///var/repo
 priority=1" > /etc/yum.repos.d/local.repo
 
 
-	mkdir -p /var/repo/extra
 	# Use downloadonly to get all the deps for important packages
 	for i in "${importantpkgs[@]}"
 	do
 		echo $i;
-		yum install --downloadonly --downloaddir=/var/repo/extra -y $i
+		yum install --downloadonly --downloaddir=/var/repo -y $i
 	done
 
 	# Get extra packages
 	for i in "${extrapkgs[@]}"
 	do
 		echo $i;
-		yum install --downloadonly --downloaddir=/var/repo/extra -y $i
+		yum install --downloadonly --downloaddir=/var/repo -y $i
 	done
 
 	# Create disabled yum repo by default
